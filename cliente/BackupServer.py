ecd8ee327f63c6e678d7e489d159d76e5a878709
Paulo <juniorcirino@gmail.com>
2018-06-21 13:07:47 -0300

    Modularização do Cliente.

--- cliente/BackupServer.py
+++ cliente/BackupServer.py
@@ -7,9 +7,6 @@ serverPort = 23000
 def main():
 	connectionSocket = None
 
-	if(checkPath("log")):
-		connection_log = open("{0}{1}{2}".format("logs", getFileSeparator(), "connection.log"))
-
 	try:
 		# Gera key
 		keyConnect = hashlib.md5("123456".encode("utf-8").strip()).hexdigest()
@@ -17,52 +14,45 @@ def main():
 
 		connectionSocket = get_connection()
 
-		print("Servidor aguardando conexão!")
+		if (connectionSocket):
 
-		keyServer = connectionSocket.recv(1024).decode("utf-8")
-		print ("Senha do Server: " + keyServer)
+			print("Servidor aguardando conexão!")
 
-		if (keyConnect == keyServer):
-			connectionSocket.send(bytes("Conectado!".encode("utf-8").strip()))
+			keyServer = connectionSocket.recv(1024).decode("utf-8")
+			print ("Senha do Server: " + keyServer)
 
-			# Abrindo arquivo para obter o checksum md5
-			arquivo = open("{0}{1}{2}{3}".format("backup",getFileSeparator(), getNameFile(), ".zip"), "rb")
+			if (keyConnect == keyServer):
+				connectionSocket.send(bytes("Conectado!".encode("utf-8").strip()))
 
-			# Obtendo hash md5 do conteudo do arquivo
-			checksum_md5 = hashlib.md5(arquivo.read()).hexdigest()
+				checksum_md5 = get_md5()
 
-			# Fechando o arquivo
-			arquivo.close()
+				dados = get_data()
 
-			dados = {
-				"ip" : "{0}".format(get_ip_address()),
-				"porta": serverPort,
-				"host_name": "linux20",
-				"name_file": "{0}".format(getNameFile()),
-				"checksum_md5": checksum_md5,
-				"date": time.strftime("%d-%m-%Y"),
-				"hour": time.strftime("%H-%M-%S")
-			}
+				json_dados = json.dumps(dados)
 
-			json_dados = json.dumps(dados)
-			print(json_dados)
-			connectionSocket.send(bytes(json_dados.encode().strip()))
+				print(json_dados)
 
-			connectionSocket.recv(1024).decode("utf-8")
-			# Abrindo arquivo para enviar
-			arquivo = open("backup{0}{1}{2}".format(getFileSeparator(), getNameFile(), ".zip"), "rb")
+				connectionSocket.send(bytes(json_dados.encode().strip()))
 
-			# Lendo os primeiros bytes do arquivo
-			l = arquivo.read(1024)
-			while(l):
-				connectionSocket.send(bytes(l))
+				connectionSocket.recv(1024).decode("utf-8")
+				# Abrindo arquivo para enviar
+				arquivo = open("backup{0}{1}{2}".format(getFileSeparator(), getNameFile(), ".zip"), "rb")
+
+				# Lendo os primeiros bytes do arquivo
 				l = arquivo.read(1024)
+				while(l):
+					connectionSocket.send(bytes(l))
+					l = arquivo.read(1024)
 
-			# Fechando o arquivo
-			arquivo.close()
+				# Fechando o arquivo
+				arquivo.close()
 
-		# Finalizando a conexão
-		connectionSocket.close()
+				# Gera o arquivo de relatório.
+				gera_log(dados["ip"], dados["porta"])
+
+			# Finalizando a conexão
+			connectionSocket.close()
+
 	except (KeyboardInterrupt, SystemExit):
 		if(connectionSocket != None):
 			connectionSocket.close()
@@ -72,13 +62,49 @@ def get_ip_address():
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
 
-def get_connection:
-	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	serverSocket.bind(("",serverPort))
-	serverSocket.listen(1)
-	connectionSocket, addr = serverSocket.accept()
+def get_connection():
+	try:
+		serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		serverSocket.bind(("",serverPort))
+		serverSocket.listen(1)
+		connectionSocket, addr = serverSocket.accept()
 
-	return connectionSocket
+		return connectionSocket
+	except (socket.error, socket.herror, socket.gaierror, socket.timeout):
+		return null
+
+def get_md5():
+	# Abrindo arquivo para obter o checksum md5
+	arquivo = open("{0}{1}{2}{3}".format("backup",getFileSeparator(), getNameFile(), ".zip"), "rb")
+
+	# Obtendo hash md5 do conteudo do arquivo
+	checksum_md5 = hashlib.md5(arquivo.read()).hexdigest()
+
+	# Fechando o arquivo
+	arquivo.close()
+
+	return checksum_md5
+
+def get_data():
+	dados = {
+		"ip" : "{0}".format(get_ip_address()),
+		"porta": serverPort,
+		"host_name": "linux20",
+		"name_file": "{0}".format(getNameFile()),
+		"checksum_md5": checksum_md5,
+		"date": time.strftime("%d-%m-%Y"),
+		"hour": time.strftime("%H-%M-%S")
+	}
+
+	return dados
+
+def gera_log(ip, porta):
+	if(checkPath("log")):
+		connection_log = open("{0}{1}{2}".format("logs", getFileSeparator(), "connection.log"))
+
+	log = "["time.strftime("%d-%m-%Y") " - " "] time.strftime("%H-%M-%S") - "ip":"porta
+
+	connection_log.write(log)
 
 if __name__ == "__main__":
 	main()
